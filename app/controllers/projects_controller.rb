66628578b05390ef898cd1262529c1411516b7aa
Murilo Aragão <murilo.matos2005@gmail.com>
2025-10-15 16:12:34 -0300

    feat: crud functions of projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,44 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:notice] = "Project was successfully created."
+      redirect_to project_path(@project)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      flash[:notice] = "Project was successfully updated."
+      redirect_to project_path(@project)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+
+    @project.destroy
+    flash[:notice] = "Project was successfully destroyed."
+    redirect_to projects_path
+  end
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
 end
